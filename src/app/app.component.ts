e5309d45297d96d3a701274037667b40ecb1ffba
Jim Compher <5629117+jastco@users.noreply.github.com>
2022-07-13 10:05:49 -0400

    Add name filter (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { User } from '@models/user';
-import { BehaviorSubject, catchError, EMPTY, Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  combineLatest,
+  debounceTime,
+  EMPTY,
+  map,
+  Observable,
+  startWith,
+  tap,
+} from 'rxjs';
 import { DataService } from './data/data.service';
 @Component({
   selector: 'app-root',
@@ -12,6 +23,10 @@ import { DataService } from './data/data.service';
 export class AppComponent implements OnInit {
   error$ = new BehaviorSubject<boolean | undefined>(undefined);
   users$: Observable<User[]> | undefined;
+  filteredUsers$: Observable<User[]> | undefined;
+  filter$: Observable<string> | undefined;
+
+  search = new FormControl('');
 
   faTriangleExclamation = faTriangleExclamation;
 
@@ -24,6 +39,20 @@ export class AppComponent implements OnInit {
         this.error$.next(true);
         return EMPTY;
       })
+    );
+
+    this.filter$ = this.search.valueChanges.pipe(
+      startWith(''),
+      debounceTime(250)
+    );
+
+    this.filteredUsers$ = combineLatest([this.users$, this.filter$]).pipe(
+      map(([users, filter]) =>
+        users.filter(
+          (user) =>
+            user.name.toLocaleLowerCase().indexOf(filter.toLowerCase()) > -1
+        )
+      )
     );
   }
 
